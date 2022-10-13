016943048c6f8d66a36784b155bc2e90f07726bf
pc20211052 <115679786+pc20211052@users.noreply.github.com>
2022-10-13 10:58:55 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 100; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.showIcon(IconNames.Heart)
+    }
+})
